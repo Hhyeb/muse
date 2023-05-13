ca5b951a4fae5ab7c837516723d65ea7017d8267
Hhyeb <ymj07141@naver.com>
2023-05-13 17:31:31 +0900

    muse

---
+++ src/layout/Body.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Body.css";
+
+class Body extends React.Component{
+    render(){
+        return(
+            <div className="app-body">
+                Body입니다.
+            </div>
+        )
+    }
+}
+export default Body;
