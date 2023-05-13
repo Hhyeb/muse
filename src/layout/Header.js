7ba7c4d2db47c3b77bb48efa18ea6b24cd52b2cb
Hhyeb <ymj07141@naver.com>
2023-05-13 23:56:46 +0900

    muse2

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,12 +1,41 @@
 import React from "react";
 import './Header.css';
+import Searchdata from "./Searchdata.json";
+import { useState } from "react";
+import { FaSearch, FaComment, FaUserAlt, FaPen } from "react-icons/fa";
 
-class Header extends React.Component{
-    render(){
+export default function Header(){
         return(
-            <div className="app-header">
-                Header입니다.
+            <div className="container">
+                <div className="app-header">
+                    <div className="c1img">
+                        <img className="museimg" alt="muse" src="img/muse.png" />
+                    </div>
+                    <div className="searchbar">
+                        <input type="text" placeholder="search..." ></input>
+                        <button>
+                            <FaSearch className="searchicon" size="20" color="black" />
+                        </button>
+                    </div>
+                    <div className="icon">
+                        <div className="pen">
+                            <button>
+                                <FaPen className="penicon" size="25" color="black" />
+                            </button>
+                        </div>
+                        <div className="user">
+                            <button>
+                                <FaUserAlt className="usericon" size="25" color="black" />
+                            </button>
+                        </div>
+                        <div className="talk">
+                            <button>
+                                <FaComment className="talkicon" size="25" color="black" />
+                            </button>
+                        </div>
+                        
+                    </div>
+                </div>
             </div>
-        )
-    }
-}export default Header;
+        );
+}
